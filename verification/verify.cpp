39788c46ee91c49ba4eb69e4474118b09a480d4d
Darragh <darraghnugent312@gmail.com>
2025-10-22 20:33:22 +1000

    Added code to verify that the output is correct

---
+++ verification/verify.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    char *name1 = argv[1];
+    char *name2 = argv[2];
+
+    FILE *file1 = fopen(name1, "r");
+    FILE *file2 = fopen(name2, "r");
+
+    double value1, value2;
+    while (fscanf(file1, "%lf", &value1) == 1 && fscanf(file2, "%lf", &value2) == 1)
+    {
+        if (fabs(value1 - value2) > 1e-6)
+        {
+            printf("Values differ: %f vs %f\n", value1, value2);
+        }
+
+        
+    }
+    printf("Comparison complete.\n");
+    return 0;
+}
